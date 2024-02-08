65a88b98ed2ce29d55e030c3c21390f7d7bb0315
AndradeItalo <ifa2@cin.ufpe.br>
2024-02-08 15:03:34 -0300

    Feat: Adicionando Entidade User e CRUD User

--- server/src/Schema/index.ts
+++ server/src/Schema/index.ts
@@ -1,11 +1,14 @@
 import { GraphQLObjectType, GraphQLSchema } from "graphql";
 import { CREATE_NEW } from "./Mutations/News";
 import { GET_ALL_NEWS } from "./Queries/News";
+import { GET_ALL_USER } from "./Queries/Users";
+import { CREATE_USER, UPDATE_USER_PASSWORD } from "./Mutations/Users";
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQuery",
   fields: {
-    getAllUsers: GET_ALL_NEWS,
+    getAllNews: GET_ALL_NEWS,
+    getAllUser: GET_ALL_USER,
   },
 });
 
@@ -13,8 +16,9 @@ const Mutation = new GraphQLObjectType({
     name: "Mutation",
     fields: {
       createNew: CREATE_NEW,
+      createUser: CREATE_USER,
       //deleteUser: DELETE_USER,
-      //updatePassword: UPDATE_PASSWORD,
+      updatePassword: UPDATE_USER_PASSWORD,
     },
   });
   
