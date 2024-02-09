e557a6d6aee9dd4e9ff34ca271ac21273d73d17f
Vitor Leitao <vml2@cin.ufpe.br>
2024-02-09 15:10:15 -0300

    feat: adicionando tela de login

--- server/src/Schema/Queries/Users.ts
+++ server/src/Schema/Queries/Users.ts
@@ -1,4 +1,4 @@
-import { GraphQLList } from "graphql";
+import { GraphQLList, GraphQLString } from "graphql";
 import { UsersType } from "../TypeDefs/Users";
 import { db } from "../../../lib/db";
 
@@ -9,3 +9,20 @@ export const GET_ALL_USER = {
       return db.user.findMany();
     },
   };
+
+  export const GET_USER_BY_NAME = {
+    type: UsersType,
+    args: {
+      username: { type: GraphQLString },
+    },
+    async resolve(parent: any, args: any) {
+      const { username } = args;
+      const userSelecionado = await db.user.findFirst({
+        where: {
+          name: username 
+        }
+      });
+      return userSelecionado;
+    },
+  };
+  
