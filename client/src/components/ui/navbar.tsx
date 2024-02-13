ec48950f250c9050f253236606d8f517779e67b6
Italo Andrade <ifa2@cin.ufpe.br>
2024-02-13 16:18:41 -0300

    Refactor: Componetizando a navbar

---
+++ client/src/components/ui/navbar.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { SettingsIcon } from "lucide-react";
+import React from "react";
+import Link from 'next/link';
+import { Inter } from 'next/font/google';
+
+
+
+export const Navbar = () => {
+    
+    return (
+        <><nav style={{ background: 'pink', color: 'white', padding: '1rem', height: '70px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', width: '100%' }}>
+          <ul style={{ listStyle: 'none', display: 'flex', flexGrow: 1, justifyContent: 'space-around' }}>
+          <Avatar>
+          <AvatarImage src="/CIN.jpg" />
+            <AvatarFallback>iti</AvatarFallback>
+          </Avatar>
+  
+          </ul>
+          <h1 style={{ color: 'black', fontSize: '24px', margin: 0 }}>Newsletter</h1> 
+          <ul style={{ listStyle: 'none', display: 'flex', flexGrow: 1, justifyContent: 'space-around' }}>
+          <DropdownMenu >
+            <DropdownMenuTrigger>
+              <SettingsIcon style={{ color: 'black', fontSize: '25px' }}/>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>Minha conta</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Perfil</DropdownMenuItem>
+              <DropdownMenuItem>Sair</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+  
+          </ul>
+        </div>
+      </nav></>
+      
+    );
+  };
